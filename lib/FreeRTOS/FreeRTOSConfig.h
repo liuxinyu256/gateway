5b494cb1fa349c3619a1e25dbad7d65e8906eea6
liu-xinyu0221 <thisdateback@outlook.com>
2026-09-01 09:19:02 +0800

    perf: reduce FreeRTOS heap to 5120 bytes after task stack cut

--- lib/FreeRTOS/FreeRTOSConfig.h
+++ lib/FreeRTOS/FreeRTOSConfig.h
@@ -26,7 +26,7 @@
 #define configTIMER_TASK_STACK_DEPTH             80
 #define configSUPPORT_STATIC_ALLOCATION         0
 #define configSUPPORT_DYNAMIC_ALLOCATION        1
-#define configTOTAL_HEAP_SIZE                   ( ( size_t ) 7168 )
+#define configTOTAL_HEAP_SIZE                   ( ( size_t ) 5120 )
 
 /* 可选 API 函数 (默认0=不编译，需显式开1) */
 #define INCLUDE_vTaskDelay                      1
