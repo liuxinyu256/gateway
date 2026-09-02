b3bb71c5158c5e83745f219146dfe9434904847f
liu-xinyu0221 <thisdateback@outlook.com>
2026-09-02 09:33:39 +0800

    fix: only start BLE module when BLE_ENABLE defined

--- app/hvac_init.c
+++ app/hvac_init.c
@@ -70,5 +70,7 @@ void hvac_start(void)
 
     init_ac_module();       /* AC 模块注册/启动 */
     debug_module_start();   /* 调试模块 */
-    ble_module_start();     /* BLE 模块（骨架） */
+#ifdef BLE_ENABLE
+    ble_module_start();     /* BLE 模块 */
+#endif
 }
